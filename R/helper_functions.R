1a99756863c56e5e74ea70f700f9b64a5a34df4f
PJ <petejones398@gmail.com>
2020-11-19 15:32:02 +0000

    add plot function to helpers

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -6,3 +6,31 @@
 get_raw <- function(...) {
   readr::read_file_raw(...)
 }
+
+#' Plot the raw vector as an image in the RStudio Viewer.
+#'
+#' Helper function to plot the raw vector as an image in the RStudio Viewer pane
+#' (or another configured browser) using the magick package.
+#'
+#' @param raw_vector The raw vector object in R which you wish to plot.
+#' @param height_px The width in pixels to scale the image before viewing. To
+#'   ensure the correct aspect ratio is maintained when specifying height, set
+#'   \code{width_px = NULL}.
+#' @param width_px The width in pixels to scale the image before viewing.
+#'
+#' @return A printable magick tibble.
+#'
+#' @examples
+#' \dontrun{
+#'   glitch_it(demo_img1, method = "far",
+#'             n_changes = 3, tune = 5) %>%
+#'   plot_vector(width_px = 300)
+#' }
+#' @export
+plot_vector <- function(raw_vector, height_px = NULL, width_px = 550) {
+  raw_vector %>%
+    magick::image_read() %>%
+    magick::image_scale(magick::geometry_size_pixels(height = height_px,
+                                                     width = width_px)) %>%
+    print()
+}
